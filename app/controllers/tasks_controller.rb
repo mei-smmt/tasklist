c1cad8aca7c46d7c2019cea88678201c7eed185a
mei <jgtop1010@gmail.com>
2020-09-24 07:02:45 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,31 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    else
+      redirect_to login_paths
+    end  
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build  # form_with 用
   end
 
   def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
-    end
+    @task = current_user.tasks.build(task_params)
+      if @task.save
+        flash[:success] = 'Task が正常に投稿されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Task が投稿されませんでした'
+        render :new
+      end
   end
 
   def edit
@@ -42,13 +46,16 @@ class TasksController < ApplicationController
      @task.destroy
      
      flash[:success] = "Task は正常に削除されました"
-     redirect_to tasks_url
+     redirect_to root_url
   end
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   # Strong Parameter
